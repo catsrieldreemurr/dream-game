2b011628eed119fa2d0198d02b0e80edee0e1b55
Vultus Limunaria <danobrovold@gmail.com>
2026-07-27 19:08:40 +0200

    Change Jump Power and Gravity

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
+# I HATE THIS 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 600.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * 1.5 * delta 
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
